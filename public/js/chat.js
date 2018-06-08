9ca3d677cf2b62e7d4477c2d7a8f1d70ab4797ba
ferrucc-io <hi@ferrucc.io>
2018-06-08 16:27:51 +0200

    Add User IDs (Client Side) 👨🏼‍💻
    Rudimental version of User Ids for anonymous users.
    
    In a production "ready" version user-id should probably tied to IP address + Cookie/Token.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   const socket = io(); // eslint-disable-line
   const form = document.getElementById('messageBox');
+  const userID = `user ${Math.round(Math.random() * 1000000)}`; // Set a username on connection -- In the future it might be a good idea to make an API call that returns the users IP address hashed
   form.addEventListener('submit', (e) => {
-    const message = document.getElementById('m').value;
-    if (message !== '') {
+    const message = {
+      body: document.getElementById('m').value,
+      sender: userID,
+    };
+    console.log(message.sender);
+    if (message.body !== '') {
       socket.emit('chat message', message);
       document.getElementById('m').value = '';
     }
@@ -11,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   socket.on('chat message', (msg) => {
     const node = document.createElement('LI');
-    const message = document.createTextNode(msg);
+    const message = document.createTextNode(`[${msg.sender}]: ${msg.body}`);
     node.appendChild(message);
     document.getElementById('messages').appendChild(node);
     window.scrollTo(0, document.body.scrollHeight);
--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 const express = require('express');
 const httpServer = require('http');
 const socketIO = require('socket.io');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 const path = require('path');
+const config = require('./config');
 
 const app = express();
 const http = httpServer.Server(app);
 const io = socketIO(http);
+const router = express.Router();
+const tokenList = {};
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+
+router.get('/', (req, res) => {
+  res.send('Ok');
+});
+
+router.post('/login', (req, res) => {
+  const postData = req.body;
+  const user = {
+    email: postData.email,
+    user: postData.user,
+  };
+  // DB authentication goes here
+  const token = jwt.sign(user, config.secret, { expiresIn: config.tokenLife });
+  const refreshToken = jwt.sign(user, config.refreshTokenSecret, { expiresIn: config.refreshTokenLife });
+  const response = {
+    status: 'Logged in',
+    token,
+    refreshToken,
+  };
+  tokenList[refreshToken] = response;
+  res.status(200).json(response);
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
